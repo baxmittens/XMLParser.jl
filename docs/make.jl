ef803102945a79b3adb8d06f529d6a70069c4c01
Bittens <Bittens.M@bgr.local>
2022-05-05 10:49:07 +0200

    ins

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,13 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, XMLParser
-makedocs(sitename="My Documentation")
+makedocs(
+	sitename = "XMLParser.jl",
+	modules = [XMLParser],
+	pages = [
+		"Home" => "index.md"
+	]
+	)
 deploydocs(
     repo = "github.com/baxmittens/XMLParser.jl.git",
+    target = "build"
 )
