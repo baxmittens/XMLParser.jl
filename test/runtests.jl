ddfbdfd4164e208bafae7b71e1f2c14387129000
Bittens <Bittens.M@bgr.local>
2023-02-22 13:44:59 +0100

    ins

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,6 @@
 using Test
 using XMLParser
 
-filename = "./test/testxml.xml"
-#filename = "./testxml.xml"
-xmlfile = read(XMLElement, filename)
-
-filename2 = "./test/tmp.xml"
-#filename2 = "./tmp.xml"
-writeXML(xmlfile, filename2)
-a = read(XMLElement, filename2)
-
 struct tmp{A,B}
 	a::A
 	b::B
@@ -28,6 +19,21 @@ struct ntmp{A,B}
 	e::tmp2
 end
 
+@testset "XMLParser.jl" begin
+
+filename = "./testxml.xml"
+#filename = "./testxml.xml"
+xmlfile = read(XMLElement, filename)
+
+@test typeof(xmlfile) == XMLElement
+
+filename2 = "./tmp.xml"
+#filename2 = "./tmp.xml"
+writeXML(xmlfile, filename2)
+a = read(XMLElement, filename2)
+
+@test typeof(a) == XMLElement
+
 obj = tmp(1.0,1)
 obj2 = ntmp(1.0,1,tmp(2.0,1),tmp(1,1.0),tmp2(3,3))
 
@@ -39,11 +45,17 @@ el2 = convert(XMLElement, obj2)
 println(el2)
 println(el2.tag.attributes)
 
-filename = "./test/tst.xml"
+filename = "./tst.xml"
 f = open(filename,"w")
 writeXMLElement(f,el2)
 close(f)
 
 el3 = read(XMLElement, filename)
 
-XML2Julia(el3)
+@test typeof(el3) == XMLElement
+
+b = XML2Julia(el3)
+
+@test typeof(b) == ntmp
+
+end
